4bfdeb886b041ce1034d2d0dc0734a095027a020
QiuDylan <dylan.qxx@gmail.com>
2023-10-14 14:22:13 -0400

    main

---
+++ question2/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sos.h>
+
+char board[50][50];
+int Player1, Player2, Computer, players, n;
+
+int main ()
+{
+        while(1)
+        {
+                how_big();
+                init_board();
+                if(numb_players()==1)
+                {
+                        players=1;
+                        comp_play();
+                }
+                else
+                {
+                        players=2;
+                        two_player();
+                }
+                if(game_done())
+                {
+                        printf("Game done!\n");
+                        winner();
+
+                        if (!play_again())
+                        {
+                                break;
+                        }
+
+                }
+        }
+        return 0;
+}
